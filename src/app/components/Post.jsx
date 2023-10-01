98a910ad272444adfff887ce8382d030d4c70f30
Eshu-M <eshambehredin20@gmail.com>
2023-10-01 14:51:03 -0700

    added the header of the post

--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import {BsThreeDots} from 'react-icons/bs'
 export default function Post({username,userImg,img,caption}) {
   return (
     <div>
-       <p>{username}</p>
+        {/* Post Header */}
+        <div className="flex items-center">
+            <img src={userImg} alt={userImg} className='h-9 rounded-full border p-1 hover:cursor-pointer'/>
+            <p className='font-bold text-xs flex-1 hover:cursor-pointer'>{username}</p>
+            <BsThreeDots className='text-xl hover:cursor-pointer'/>
+        </div>
     </div>
   )
 }
