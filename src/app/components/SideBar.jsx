70c80fcff42744d4d2f382e9a0d43b61b56be48f
Eshu-M <eshambehredin20@gmail.com>
2023-10-01 10:36:34 -0700

    updated the sidebar and made it responsive

--- src/app/components/SideBar.jsx
+++ src/app/components/SideBar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import {GoHomeFill} from 'react-icons/go'
 import {BsSearch} from 'react-icons/bs'
 import {AiOutlineHeart ,AiOutlineMenu} from 'react-icons/ai'
@@ -9,53 +10,57 @@ import {TfiVideoClapper} from 'react-icons/tfi'
 import {LuSend} from 'react-icons/lu'
 import {HiAtSymbol} from 'react-icons/hi'
 export default function SideBar() {
+   const [selectedOption ,setSelectedOption]=useState('')
+   function isSelected(option){
+      setSelectedOption(option)
+   }
   return (
     <>
-    <div className='flex flex-col mt-7'>
-      <div className='sideMenu '>
+    <div className='flex flex-col mt-7 menuOption'>
+      <div onClick={()=>isSelected('Option1')} className={selectedOption === 'Option1' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <GoHomeFill className='icons'/>
-         <h1>Home</h1>
+         <h1 className='optionNames'>Home</h1>
       </div>
-      <div className='sideMenu'>
+      <div onClick={()=>isSelected('Option2')} className={selectedOption === 'Option2' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <BsSearch className='icons'/>
-         <h1 >Search</h1>
+         <h1 className='optionNames'>Search</h1>
       </div>
-      <div className='sideMenu'>
+      <div onClick={()=>isSelected('Option3')} className={selectedOption === 'Option3' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <ImCompass2 className='icons'/>
-         <h1 >Explore</h1>
+         <h1 className='optionNames'>Explore</h1>
       </div>
-      <div className='sideMenu'>
+      <div onClick={()=>isSelected('Option4')} className={selectedOption === 'Option4' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <TfiVideoClapper className='icons'/>
-         <h1>Reels</h1>
+         <h1 className='optionNames'>Reels</h1>
       </div>
-      <div className='sideMenu'>
+      <div onClick={()=>isSelected('Option5')} className={selectedOption === 'Option5' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <LuSend className='icons'/>
-         <h1 >Message</h1>
+         <h1 className='optionNames'>Message</h1>
       </div>
-      <div className='sideMenu'>
+      <div onClick={()=>isSelected('Option6')} className={selectedOption === 'Option6' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <AiOutlineHeart className='icons'/>
-         <h1 >Notification</h1>
+         <h1 className='optionNames'>Notification</h1>
       </div>
-      <div className='sideMenu'>
+      <div onClick={()=>isSelected('Option7')} className={selectedOption === 'Option7' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <SiAddthis className='icons'/>
-         <h1 >Create</h1>
+         <h1 className='optionNames'>Create</h1>
       </div>
-      <div className='sideMenu'>
+      <div onClick={()=>isSelected('Option8')} className={selectedOption === 'Option8' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <CgProfile className='icons'/>
-         <h1 >Profile</h1>
+         <h1 className='optionNames'>Profile</h1>
       </div>
-      
     </div>
-    <div className="flex flex-col mt-5 absolute bottom-0 mb-5">
-    <div className='sideMenu'>
+      <div className='mt-10 absolute bottom-0 mb-5 flex flex-col menuOption'>
+
+      <div onClick={()=>isSelected('Option9')} className={selectedOption === 'Option9' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <HiAtSymbol className='icons'/>
-         <h1 >Treads</h1>
+         <h1 className='optionNames'>Treads</h1>
       </div>
-      <div className='sideMenu'>
+      <div onClick={()=>isSelected('Option10')} className={selectedOption === 'Option10' ? 'sideMenuItemsSelected sideMenu' : 'sideMenu'}>
          <AiOutlineMenu className='icons'/>
-         <h1 >More</h1>
+         <h1 className='optionNames'>More</h1>
       </div>
-    </div>
+      </div>
     </>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import SideBar from './components/SideBar'
+import Header from './components/Header'
 
 export default function Home() {
   return (
-    <div className='flex'>
-    <section className='bg-white h-[100vh] max-h-[100vh] w-64 p-5 border-r-2 border-gray-100 pt-10 pl-6  left-0 sticky'>
-      <header>
-       <img className='h-8 w-22' src="https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.png" alt="instagram logo" />
-      </header>
+    <div className='flex flex-row'>
+      <div className='md:hidden p-5 border-b w-full'>
+        <Header/>
+      </div>
+    <section className='bg-white h-[100vh] max-h-[100vh] w-fit xl:w-[16%] p-5 border-r-2 border-gray-100 pt-10 pl-6  left-0 sticky md:inline-block hidden'>
+       <Header/>
        <SideBar/>
     </section>
     <section>
